83f91152ebc4fd42e1a17fdbec7b11484f04b09e
Alex <galkinamaria37@gmail.com>
2023-11-30 20:12:56 +0500

    career

--- app/js/app.js
+++ app/js/app.js
@@ -511,5 +511,10 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     
-    
+    let burgerIcon = document.querySelector('.burger__icon');
+    let burgerMenu = document.querySelector('.burger__menu');
+
+    burgerIcon.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+    })
 });
--- dist/js/main.js
+++
@@ -1,6 +0,0 @@
-let burgerIcon = document.querySelector('.burger__icon');
-let burgerMenu = document.querySelector('.burger__menu');
-
-burgerIcon.addEventListener('click', () => {
-    burgerMenu.classList.toggle('active');
-})
