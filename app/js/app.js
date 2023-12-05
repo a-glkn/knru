2bf2b99efe1edeb0d3b91de51dc74b35d73d3772
Alex <galkinamaria37@gmail.com>
2023-12-06 00:36:54 +0500

    career & vacancy pages

--- app/js/app.js
+++ app/js/app.js
@@ -509,12 +509,4 @@ document.addEventListener('DOMContentLoaded', () => {
             error.innerText = "";
         };
     }
-
-    
-    let burgerIcon = document.querySelector('.burger__icon');
-    let burgerMenu = document.querySelector('.burger__menu');
-
-    burgerIcon.addEventListener('click', () => {
-        burgerMenu.classList.toggle('active');
-    })
 });
